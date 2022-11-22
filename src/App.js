dcaf08713e34f0357e34b60ba990a875256ca860
Hary88788 <harut155794@gmail.com>
2022-11-22 17:10:18 +0400

    homework 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {MyName,WhoIAm} from './component.js';
+
+
+function Greeting () {
+
+  return (
+    <div>
+      <h4>Hello, i am a function component</h4>
+    </div>
+  )
+}
+
+function Name (props){
+
+  return(
+    <span>{props.name}</span>
+  )
+}
+
+function Surname (props){
+
+  return(
+    <span>{props.surname}</span>
+  )
+}
+
+
+function User (props) {
+
+  return (
+      <h2>Hello, i am React developer <Name name={props.name}/> <Surname surname="Hovhannisyan" />, and  i am {props.age}</h2>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
+      <MyName />
+      <WhoIAm />
+    
+      <Greeting />
+
+        <User name = "Harut" age = {28} />
+        <User name = "Arman" age = {27} />
+       
       </header>
     </div>
   );
---
+++ src/component.js
@@ -0,0 +1,22 @@
+function MyName () {
+
+    return(
+        <div>
+            <h1>Harut Hovhannisyan</h1>
+        </div>
+    )
+}
+
+function WhoIAm () {
+
+    return(
+    <div>
+        <p>
+           I am Harut Hovhannisyan LIVE in Vanadzor,  I am React developer,I LIKE development,i am have a brodher her name is Arman Hovhannisyan he is also a programmer,he works for php and nodejs
+        </p>    
+    </div> )
+}
+
+export{
+    MyName,WhoIAm,
+}
